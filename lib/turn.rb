9abd5e5cf8e712736051d269020babfc3b22368c
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-26 03:02:58 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def display_board(board)
+  puts(" #{board[0]} | #{board[1]} | #{board[2]} ")
+  puts("-----------")
+  puts(" #{board[3]} | #{board[4]} | #{board[5]} ")
+  puts("-----------")
+  puts(" #{board[6]} | #{board[7]} | #{board[8]} ")
+end
+
+def position_taken?(board, index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if(index.between?(0,8) && !position_taken?(board, index))
+    return true
+  else
+    return false
+  end
+end
+
+def input_to_index(position)
+  if(position.to_i != 0)
+    index = position.to_i - 1
+    return index
+  else
+    return -1
+  end
+end
+
+def move(board, index, value = "X")
+  
+end
+
+def turn(board)
+end
+
